02420b12ebf5024ce088b865ba1d7ac9f751411f
sw9t <warfatre@gmail.com>
2017-05-22 21:32:46 +0300

    добавлена неограниченная очередь

--- Smo/SSO.cs
+++ Smo/SSO.cs
@@ -46,6 +46,82 @@ namespace Smo
             return label;
         
         }
+        public static string Smo_s_neogranicennoi(double lambda, double t_obslujivania, double max_oceredi_r,double n_kanalov)
+        {
+            string label;
+            double r;
+            double ro,ro_stationar;
+            double[] massP = new double[20];
+            double temp = 1;
+            ro = lambda * t_obslujivania;
+            ro_stationar = ro / n_kanalov;            
+            
+            if (ro_stationar>=1)
+	        {
+	        	  return "При заданном количестве каналов не существует стационарный режим.";
+	        }
+            for (int i = 1; i <= n_kanalov+1; i++)
+            {
+                temp += Math.Pow(ro, i) / factorial(i);
+            }
+            massP[0]=1/temp;
+            for (int i = 1; i <= n_kanalov; i++)
+            {
+                massP[i] = Math.Pow(ro, i + 1) / factorial(i) * massP[0];
+            }
+            r=(Math.Pow(ro,n_kanalov+1)*massP[0])/(n_kanalov*factorial((int)n_kanalov)*Math.Pow(1-ro/n_kanalov,2));
+
+            return label;
+        
+        }
+        private static string Smo_s_neogranicennoi_label(double lambda, double t_obslujivania, double max_oceredi_r, double n_kanalov, double[] massP, double r, double ro, double ro_stationar)
+        {
+            string label;
+            label = "Ответ: ";
+            label += "\r\nρ=λ*t_обслуживания= " + ro.ToString();
+            label += "\r\n\r\nРассмотрим СМО с неограниченной очередью, при n= " + n_kanalov.ToString();
+            label += "\r\nρ_=ρ/n = " + ro_stationar.ToString();
+            label += "\r\nρ_<1 следовательно стационарный режим есть";
+            label += "\r\nВозможные состояния системы: ";
+            label += "\r\n\tX0 - все " + n_kanalov.ToString() + " каналов не работают, свободны;";
+            for (int i = 1, j = (int)n_kanalov; i < n_kanalov; i++, j--)
+            {
+                label += "\r\n\tX" + i.ToString() + " - " + i.ToString() + " каналов работают, " + j.ToString() + " каналов свободны;";
+            }
+            label += "\r\n\tX" + n_kanalov.ToString() + "- все " + n_kanalov.ToString() + " каналов работают,заняты;";
+            for (int i = (int)n_kanalov+1; i < (int)n_kanalov+2; i++)
+            {
+                label += "\r\n\tX" + i.ToString() + " - " + n_kanalov.ToString() + " каналов работают, " + i.ToString() + " заявка в очереди;";
+            }
+            label += "\r\n\t.........................................................\r\n";
+
+            label += "\r\n\r\nНайдем вероятности состояний:";
+            for (int i = 0; i <= n_kanalov; i++)
+            {
+                label += "\r\nP" + i.ToString() + "=" + massP[i].ToString();
+            }
+            label += "\r\nВероятность отказа: P_отказа=0";
+            label += "\r\nВероятность обслуживания: P_обслуживания=1";
+            label += "\r\nСредняя длина очереди:r_="+r.ToString();
+            label += "\nАбсолютная пропускная  способность";
+            label += "\r\nА=λ=" + lambda.ToString();
+            label += "\r\nСреднее число занятых касс";
+            label += "\r\nnз=ρ " + ro.ToString();
+            label += "\r\nСреднее число свободных касс";
+            label += "\r\nn_св= " + (n_kanalov-ro).ToString();
+            label += "\r\nКоэффициенты загрузки";
+            label += "\r\nk_з= " + (ro/n_kanalov).ToString();
+            label += "\r\nСреднее время ожидания";
+            label += "\r\nt_ожидания_среднее= " + (r/lambda).ToString();
+            label+="\r\nСреднее время пребывания с учетом ожидания и обслуживания"ж
+            label+="t_системы=t_ожидания+t_обслуживания"+(r/lambda+t_obslujivania);
+
+            return label;
+
+        }
+
+
+
         private static string Smo_s_otkazami_label(double Q, double g, double t_obslujivanie, double d, double day, double n_kanalov, double lambda, double miu, double ro, double[] array_P_i, double temp,
             double P_otkaza, double P_obsluhivania, double A, double N_zaneatih, double N_svobodnih, double K_zan, double K_prost, double s)
         {
@@ -59,9 +135,9 @@ namespace Smo
             {
                 label += "\r\n\tX"+i.ToString()+" - "+i.ToString()+" каналов работают, "+j.ToString()+" каналов свободны;";  
             }
-            label += "\r\n\tX" + n_kanalov.ToString() + "- все " + n_kanalov.ToString() + " каналов не работают, свободны;";
+            label += "\r\n\tX" + n_kanalov.ToString() + "- все " + n_kanalov.ToString() + " каналов работают, заняты;";
             label+="\r\n\r\nНайдем вероятности состояний:";
-            for (int i = 0; i < n_kanalov; i++)
+            for (int i = 0; i <= n_kanalov; i++)
 			{
                 label += "\r\nP" + i.ToString() + "=" + array_P_i[i].ToString();
 			}
