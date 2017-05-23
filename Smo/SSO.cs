4113e9b48794f9b4363f72ee01901a90dc699436
sw9t <warfatre@gmail.com>
2017-05-23 17:42:42 +0300

    доработано

--- Smo/SSO.cs
+++ Smo/SSO.cs
@@ -46,24 +46,95 @@ namespace Smo
             return label;
         
         }
-        public static string Smo_s_neogranicennoi(double lambda, double t_obslujivania, double max_oceredi_r,double n_kanalov)
+        public static string Smo_s_ogranicennoi_ocerediu(double lambda, double t_obslujivanie, double n_kanalov,double m_kanalov)
+        {
+            double ro = lambda * t_obslujivanie;
+            double[] array_P_i = new double[20];
+            double temp = 1;
+            double t_sistema;
+            double P_otkaza, P_obsluhivania, A, N_zaneatih, N_svobodnih, K_zan, K_prost, s,r=0,t_ojidania;
+            for (int i = 1; i <= n_kanalov; i++)
+            {
+                temp += Math.Pow(ro, i) / factorial(i);
+            }
+            array_P_i[0] = 1 / temp;
+            for (int i = 1; i <= n_kanalov; i++)
+            {
+                array_P_i[i] = Math.Pow(ro, i) / factorial(i) * array_P_i[0];
+            }
+            for (int i = (int)n_kanalov+1,j=1; i <= n_kanalov+m_kanalov; i++,j++)
+            {
+                array_P_i[i] = Math.Pow(ro, i) / (factorial((int)n_kanalov)*Math.Pow(n_kanalov,j) )* array_P_i[0];
+            }
+            P_otkaza = array_P_i[(int)(n_kanalov + m_kanalov)];
+            P_obsluhivania = 1 - P_otkaza;
+            A = lambda * P_obsluhivania;
+            N_zaneatih = A * t_obslujivanie;
+            N_svobodnih = n_kanalov - N_zaneatih;
+            K_zan = N_zaneatih / n_kanalov;
+            K_prost = 1 - K_zan;
+            for (int i = (int)n_kanalov + 1, j = 1; i <= n_kanalov + m_kanalov; i++, j++)
+            {
+                r += j * array_P_i[i];
+            }
+            t_ojidania = r / lambda;
+            t_sistema = t_ojidania + t_obslujivanie * P_obsluhivania;
+            string label = Smo_s_otkazami_label(Q, g, t_obslujivanie, d, day, n_kanalov, lambda, miu, ro, array_P_i, temp, P_otkaza, P_obsluhivania, A, N_zaneatih, N_svobodnih, K_zan, K_prost, s);
+            return label;
+        }
+        private static string Smo_s_ocerediu_label(double lambda, double t_obslujivanie, double n_kanalov,double m_kanalov,double ro,double []array_Pi,double t_sistema,double t_ojidania,double p_otkaza,double p_obsl,double A,double N_zaneatih,double N_svobodnih,double K_zan,double K_prost,double r)
         {
             string label;
+            label = "Ответ:\r\nЧисло партий в день:\r\nλ=Q/(g*day)=  " + lambda.ToString();
+            label += "\r\nρ=λ*t_обслуживания= " + ro.ToString();
+            label += "\r\n\r\nРассмотрим СМО с отказами с числом каналов n= " + n_kanalov.ToString();
+            label += "\r\nВозможные состояния системы: ";
+            label += "\r\n\tX0 - все "+n_kanalov.ToString()+" каналов не работают, свободны;";  
+            for (int i = 1,j=(int)n_kanalov-1; i < n_kanalov; i++,j--)
+            {
+                label += "\r\n\tX"+i.ToString()+" - "+i.ToString()+" каналов работают, "+j.ToString()+" каналов свободны;";  
+            }
+            label += "\r\n\tX" + n_kanalov.ToString() + "- все " + n_kanalov.ToString() + " каналов работают, заняты;";
+            label+="\r\n\r\nНайдем вероятности состояний:";
+            for (int i = 0; i <= n_kanalov; i++)
+			{
+                label += "\r\nP" + i.ToString() + "=" + array_P_i[i].ToString();
+			}
+            label += "\r\nВероятность отказа: P_отказа=P_" + n_kanalov.ToString() + "=" +P_otkaza;
+            label += "\r\nВероятность обслуживания: P_обслуживания=1-Р_отказа="+P_obsluhivania+"="+Math.Round(P_obsluhivania*100)+"%";
+            label += "\nНайдем абсолютную пропускную способность";
+            label += "\r\nА= " + A.ToString();
+            label  += "\r\nСреднее число занятых площадок";
+            label  += "\r\nnз= " + N_zaneatih.ToString();
+            label  += "\r\nСреднее число свободных площадок";
+            label += "\r\nnсв= " + N_svobodnih.ToString();
+            label  += "\r\nКоэффициенты загрузки";
+            label  += "\r\nkз= " + K_zan.ToString();
+            label  += "\r\nkпр= " + K_prost.ToString();
+            label  += "\r\nПолезная площадь склада";
+            label  += "\r\ns= " + s.ToString();
+
+            return label;
+        }
+        public static string Smo_s_neogranicennoi(double lambda, double t_obslujivania, double max_oceredi_r,double n_kanalov)
+        {
+            string label="";
             double r;
             double ro,ro_stationar;
             double[] massP = new double[20];
             double temp = 1;
             ro = lambda * t_obslujivania;
             ro_stationar = ro / n_kanalov;            
-            
             if (ro_stationar>=1)
 	        {
 	        	  return "При заданном количестве каналов не существует стационарный режим.";
 	        }
-            for (int i = 1; i <= n_kanalov+1; i++)
+            for (int i = 1; i <= n_kanalov; i++)
             {
                 temp += Math.Pow(ro, i) / factorial(i);
             }
+            double temp1 = Math.Pow(ro, n_kanalov + 1) / (factorial((int)(n_kanalov))*(n_kanalov-ro));
+            temp += temp1;
             massP[0]=1/temp;
             for (int i = 1; i <= n_kanalov; i++)
             {
@@ -71,20 +142,21 @@ namespace Smo
             }
             r=(Math.Pow(ro,n_kanalov+1)*massP[0])/(n_kanalov*factorial((int)n_kanalov)*Math.Pow(1-ro/n_kanalov,2));
 
-            return label;
+            return label=Smo_s_neogranicennoi_label(lambda,t_obslujivania,max_oceredi_r,n_kanalov,massP,r,ro,ro_stationar,temp1);
         
         }
-        private static string Smo_s_neogranicennoi_label(double lambda, double t_obslujivania, double max_oceredi_r, double n_kanalov, double[] massP, double r, double ro, double ro_stationar)
+        private static string Smo_s_neogranicennoi_label(double lambda, double t_obslujivania, double max_oceredi_r, double n_kanalov, double[] massP, double r, double ro, double ro_stationar,double temp1)
         {
             string label;
             label = "Ответ: ";
+            label += temp1.ToString();
             label += "\r\nρ=λ*t_обслуживания= " + ro.ToString();
             label += "\r\n\r\nРассмотрим СМО с неограниченной очередью, при n= " + n_kanalov.ToString();
             label += "\r\nρ_=ρ/n = " + ro_stationar.ToString();
             label += "\r\nρ_<1 следовательно стационарный режим есть";
             label += "\r\nВозможные состояния системы: ";
             label += "\r\n\tX0 - все " + n_kanalov.ToString() + " каналов не работают, свободны;";
-            for (int i = 1, j = (int)n_kanalov; i < n_kanalov; i++, j--)
+            for (int i = 1, j = (int)n_kanalov-1; i < n_kanalov; i++, j--)
             {
                 label += "\r\n\tX" + i.ToString() + " - " + i.ToString() + " каналов работают, " + j.ToString() + " каналов свободны;";
             }
@@ -113,7 +185,7 @@ namespace Smo
             label += "\r\nk_з= " + (ro/n_kanalov).ToString();
             label += "\r\nСреднее время ожидания";
             label += "\r\nt_ожидания_среднее= " + (r/lambda).ToString();
-            label+="\r\nСреднее время пребывания с учетом ожидания и обслуживания"ж
+            label += "\r\nСреднее время пребывания с учетом ожидания и обслуживания";
             label+="t_системы=t_ожидания+t_обслуживания"+(r/lambda+t_obslujivania);
 
             return label;
@@ -131,7 +203,7 @@ namespace Smo
             label += "\r\n\r\nРассмотрим СМО с отказами с числом каналов n= " + n_kanalov.ToString();
             label += "\r\nВозможные состояния системы: ";
             label += "\r\n\tX0 - все "+n_kanalov.ToString()+" каналов не работают, свободны;";  
-            for (int i = 1,j=(int)n_kanalov; i < n_kanalov; i++,j--)
+            for (int i = 1,j=(int)n_kanalov-1; i < n_kanalov; i++,j--)
             {
                 label += "\r\n\tX"+i.ToString()+" - "+i.ToString()+" каналов работают, "+j.ToString()+" каналов свободны;";  
             }
@@ -142,7 +214,7 @@ namespace Smo
                 label += "\r\nP" + i.ToString() + "=" + array_P_i[i].ToString();
 			}
             label += "\r\nВероятность отказа: P_отказа=P_" + n_kanalov.ToString() + "=" +P_otkaza;
-            label += "\r\nВероятность обслуживания: P_обслуживания=1-Р_отказа="+P_obsluhivania+"="+Math.Round(P_obsluhivania*100);
+            label += "\r\nВероятность обслуживания: P_обслуживания=1-Р_отказа="+P_obsluhivania+"="+Math.Round(P_obsluhivania*100)+"%";
             label += "\nНайдем абсолютную пропускную способность";
             label += "\r\nА= " + A.ToString();
             label  += "\r\nСреднее число занятых площадок";
---
+++ smo_gui/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("smo_gui")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("smo_gui")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ae52c28c-750f-46c3-9ec2-361dca8bf8d8")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
